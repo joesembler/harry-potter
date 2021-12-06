1ac29d76d358d8b05d37eceb6408ca382996817d
Joe Sembler <joeysembler@gmail.com>
2021-12-05 21:42:13 -0500

    made placeholder character fetch function work. added a background image and added information below the character display

--- script.js
+++ script.js
@@ -2,18 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    const mainImage = document.getElementById('main-image');
-    console.log(mainImage);
-
+    
+    // const mainDisplay = document.getElementById("main-display");
 
     fetch("http://hp-api.herokuapp.com/api/characters")
         .then(resp => resp.json())
         .then(data => {
-            const randomInt = getRandomInt(291);
-            console.log(data);
-            console.log(data[randomInt].image);
-            mainImage.src = data[randomInt].image;
-            console.log(mainImage.src);
+            displayCharacter(data);
         })
 })
 
@@ -21,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function() {
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+
+function displayCharacter(data) {
+    const mainImage = document.getElementById('main-image');
+    const randomInt = getRandomInt(25);
+    const character = data[randomInt];
+    const characterName = document.querySelector('#main-display h3');
+    const characterHouse = document.getElementById('house');
+    const characterAncestry = document.getElementById('ancestry');
+    const mainList = document.querySelector('#main-display ul');
+
+            
+
+    mainImage.src = character.image;
+            
+            
+    characterName.innerHTML = character.name;
+            
+    characterHouse.innerHTML = "House: " + character.house;
+    characterAncestry.innerHTML = "Ancestry: " + character.ancestry;
+}
